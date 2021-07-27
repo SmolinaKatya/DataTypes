59fd216bb5ecedbd6429937a55768610859f106c
SmolinaKatya <katerina-ketty@list.ru>
2021-07-27 22:00:26 +0300

    adding files

---
+++ StopWatch/StopWatch/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var PackageTime = System.Diagnostics.Stopwatch.StartNew();
+            PackageTime.Start();
+            int i = 123;
+            object o = i;
+            PackageTime.Stop();
+            Console.WriteLine(PackageTime.Elapsed);
+            var UnpackageTime = System.Diagnostics.Stopwatch.StartNew();
+            UnpackageTime.Start();
+            i = (int)o;
+            UnpackageTime.Stop();
+            Console.WriteLine(UnpackageTime.Elapsed);
+
+        }
+    }
+}
