df66da147f6a565f2d871300acbeb25c71118118
SmolinaKatya <katerina-ketty@list.ru>
2021-07-29 15:17:39 +0300

    files changed

--- DataTypesSamples/Program.cs
+++ DataTypesSamples/Program.cs
@@ -13,10 +13,13 @@ namespace DataTypesSamples
             FigureStruct figure1 = new FigureStruct() { NumberOfSides = 10, SideLength = 3 };
             CountArea(figure);
             Console.WriteLine("Number of sides = {0}, side length = {1}, area = {2:N2}", figure.NumberOfSides, figure.SideLength, figure.Area);
-            CountArea1(ref figure);
-            Console.WriteLine("Number of sides = {0}, side length = {1}, area = {2:N2}", figure.NumberOfSides, figure.SideLength, figure.Area);
+            double Area, Area1;
+            CountArea1(figure, out Area);
+            Console.WriteLine("Number of sides = {0}, side length = {1}, area = {2:N2}", figure.NumberOfSides, figure.SideLength, Area);
             CountArea2(ref figure1);
             Console.WriteLine("Number of sides = {0}, side length = {1}, area = {2:N2}", figure1.NumberOfSides, figure1.SideLength, figure1.Area);
+            CountArea3(figure1, out Area1);
+            Console.WriteLine("Number of sides = {0}, side length = {1}, area = {2:N2}", figure1.NumberOfSides, figure1.SideLength, Area1);
         }
 
         public static object CountArea(Figure figureOrig)
@@ -25,17 +28,24 @@ namespace DataTypesSamples
             return figureOrig;
         }
 
-        public static object CountArea1(ref Figure figureRef)
+        public static object CountArea1(Figure figureRef, out double AreaClass)
         {
             figureRef = new Figure() { NumberOfSides = 7, SideLength = 5 };
             figureRef.Area = figureRef.NumberOfSides * Math.Pow(figureRef.SideLength, 2) / (4 * Math.Tan(Math.PI / figureRef.NumberOfSides));
-            return figureRef.Area;
+            return AreaClass = figureRef.Area;
         }
 
         public static object CountArea2(ref FigureStruct figureOrig)
         {
             figureOrig.Area = figureOrig.NumberOfSides * Math.Pow(figureOrig.SideLength, 2) / (4 * Math.Tan(Math.PI / figureOrig.NumberOfSides));
             return figureOrig;
+        }
+
+        public static object CountArea3(FigureStruct figureRef, out double AreaStruct)
+        {
+            figureRef = new FigureStruct() { NumberOfSides = 7, SideLength = 9 };
+            figureRef.Area = figureRef.NumberOfSides * Math.Pow(figureRef.SideLength, 2) / (4 * Math.Tan(Math.PI / figureRef.NumberOfSides));
+            return AreaStruct = figureRef.Area;
         }
     }
 }
--- StopWatch/StopWatch/Program.cs
+++ StopWatch/StopWatch/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
 using System.Text;
 
 namespace StopWatch
@@ -9,18 +10,24 @@ namespace StopWatch
     {
         static void Main(string[] args)
         {
-            var PackageTime = System.Diagnostics.Stopwatch.StartNew();
-            PackageTime.Start();
-            int i = 123;
-            object o = i;
-            PackageTime.Stop();
-            Console.WriteLine(PackageTime.Elapsed);
-            var UnpackageTime = System.Diagnostics.Stopwatch.StartNew();
-            UnpackageTime.Start();
-            i = (int)o;
-            UnpackageTime.Stop();
-            Console.WriteLine(UnpackageTime.Elapsed);
-
+            double sum1 = 0;
+            double sum2 = 0;
+            for (int i = 0; i <= 100; i++)
+            {
+                var PackageTime = System.Diagnostics.Stopwatch.StartNew();
+                PackageTime.Start();
+                object o = i;
+                PackageTime.Stop();
+                sum1 += PackageTime.ElapsedTicks;
+                var UnpackageTime = System.Diagnostics.Stopwatch.StartNew();
+                UnpackageTime.Start();
+                i = (int)o;
+                UnpackageTime.Stop();
+                sum2 += UnpackageTime.ElapsedTicks;
+            }
+            Console.WriteLine("{0:0.0000}", sum1/100);
+            Console.WriteLine("{0:0.0000}", sum2/100);
         }
+         
     }
 }
